ce8f698deef559e22be0245589847802fcf4af98
TheDongTran <48229237+ITheDongTran@users.noreply.github.com>
2022-08-31 01:30:46 +0000

    Added main function for checking user messages for checking posture and random other messages as well.

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import discord
+import os
+import random
+TOKEN = os.environ['TOKEN']
 
-TOKEN = 'MTAxMTE0NDA4NDM1NTgwOTI5Mg.GS4Quv.XPKTBiwWBdeV5t70ZMg6N3zcIuUSjNlzqjAPTA'
+client = discord.Client(intents=discord.Intents.all())
 
-client = discord.Client(intents=discord.Intents.default())
+posture_words = ['sit', 'chair', 'stand', 'desk', 'posture', 'stretch', 'break', 'tired']
+
+posture_check = ["Posture Check!", "Check Your Posture!", "Everyone Sit Up!", "Stand up for a bit?"]
 
 # when bot is ready sends message 
 @client.event
 async def on_ready():
   print(f'We have logged in as {client.user}')
 
-# # runs when bot receives message 
-# @client.event
-# async def on_message(message):
-#   if message.author == client.user:
-#     return
+# runs when bot receives message
+@client.event
+async def on_message(message):
+  channel = message.channel
+  msg = message.content
 
-#   if message.content.startswith('$hello'):
-#     await message.channel.send('Hello!')
+  if msg.startswith("$hello"):
+    await channel.send(f'Hello {message.author}!')
 
-# client.run(TOKEN)
+  elif msg.startswith("is cassy a furry?"):
+    await channel.send('Yes 😔')
+  
+  elif msg.startswith("is dong a furry?"):
+    await channel.send('Hell nah he is a gigachad ezclap 😎')
+
+  elif msg.startswith("are you telling the truth?"):
+    await channel.send('Of course I am. I only speak the truth, no kissy.')
+
+  if any(word in msg for word in posture_words):
+    await channel.send(random.choice(posture_check))
+
+client.run(TOKEN)
